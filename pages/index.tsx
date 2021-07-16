537afbc15d4797cfac6bfc4e49744facde6b387b
Daniel Souza <daniielsouzapvh@gmail.com>
2021-07-16 15:57:30 -0300

    fix: chang prod url

--- pages/index.tsx
+++ pages/index.tsx
@@ -274,7 +274,7 @@ export async function getServerSideProps(context: any) {
   const cookies = nookies.get(context)
   const token = cookies.USER_TOKEN
     
-  const { isAuthenticated }  = await fetch(`http://localhost:3000/api/auth`, {
+  const { isAuthenticated }  = await fetch(`${PROD_URL}/api/auth`, {
     headers: {
       Authorization: token
     }
